7cfce833e67e21c7adb84d00b70c8e6bda5be6f5
Bearded Avenger <email@nickhaskins.com>
2015-05-20 11:51:34 -0500

    ajax call to log progress

--- admin/includes/process.data.php
+++ admin/includes/process.data.php
@@ -7,9 +7,46 @@
 */
 class cgcVideoTrackingProcess {
 
+	function __construct(){
 
-	function __construct(){}
+		add_action('wp_ajax_process_share_love',				array($this,'add_progress'));
 
-	function record(){}
+	}
+
+	/**
+	*	Process ajax call and add progress
+	*
+	*	@since 5.0
+	*/
+	function add_progress(){
+
+		if ( isset( $_POST['action'] ) && $_POST['action'] == 'process_video_progress' ) {
+
+	    	if ( wp_verify_nonce( $_POST['nonce'], 'process-video-progress' ) ) {
+
+	    		$user_id 	= is_user_logged_in() ? get_current_user_id() : false;
+	    		$video_id   = isset( $_POST['video_id'] ) ? $_POST['video_id'] : false;
+	    		$percent    = isset( $_POST['percent'] ) ? $_POST['percent'] : false;
+
+	    		$progress  = cgc_video_tracking_add_progress( sanitize_text_field( $video_id ), filter_var( $percent, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION ) );
+
+	    		if ( !is_wp_error( $progress ) ) {
+
+		       		wp_send_json_success();
+
+	    		} else {
+
+	    			wp_send_json_error();
+
+	    		}
+		    }
+
+	  	} else {
+
+	  		wp_send_json_error();
+
+	  	}
+
+	}
 }
 new cgcVideoTrackingProcess();
