85f72e7c3a9df44d9973930342c1e6f4c288dc2d
Bearded Avenger <email@nickhaskins.com>
2015-09-08 17:06:19 -0400

    allow users to mark videos as watched or unwatched
    https://github.com/CGCookie/cgcookie-5.0/issues/475

--- includes/helpers.php
+++ includes/helpers.php
@@ -1,4 +1,23 @@
 <?php
+
+/**
+*	Drop the progress of a specific video for a specific user
+*	@param $args array an array of data to be recorded into the database
+*	@since 5.0
+*/
+function cgc_video_tracking_remove_progress( $video_id = 0 ){
+
+	// bail out if we dont' have a video id or percent
+	if ( empty( $video_id ) )
+		return;
+
+	$user_id = get_current_user_ID();
+
+	$db = new CGC_Video_Tracking_DB;
+
+	$db->drop_progress( $video_id, $user_id );
+}
+
 /**
 *	Add the percentage of a video watched for a specific user
 *	@param $args array an array of data to be recorded into the database
